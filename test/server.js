8529b695772b0323ffd58dbb20c62c1ff1d87942
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2024-08-20 01:58:06 +0000

    updated path

--- test/server.js
+++ test/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
-// This is your test secret API key.
 const Stripe = require("stripe");
 const stripe = Stripe('sk_test_51ONR74CREOY0emWDVLhcbnHir4JXUaLh1KeGlPDh8E73t5Hz6N8ED5ocG24UVe7rrS99kDiuq0E5c08FpLO7RO0v00XKHwv2Av');
 const OnrampSessionResource = Stripe.StripeResource.extend({
   create: Stripe.StripeResource.method({
     method: 'POST',
-    path: 'crypto/onramp_sessions',
+    path: '/crypto/onramp_sessions',
   }),
 });
 
@@ -14,10 +13,9 @@ const OnrampSessionResource = Stripe.StripeResource.extend({
 app.use(express.static("public"));
 app.use(express.json());
 
-app.post("/create-onramp-session", async (req, res) => {
+app.post("/test/create-onramp-session", async (req, res) => {
   const { transaction_details } = req.body;
 
-  // Create an OnrampSession with the order amount and currency
   const onrampSession = await new OnrampSessionResource(stripe).create({
     transaction_details: {
       destination_currency: transaction_details["destination_currency"],
